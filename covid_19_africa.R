7b9ec61cf18f879b5efa3b15670399e3fcc5518a
julokr <amjuliusokwir@gmail.com>
2020-05-08 10:46:45 +0300

    "2020/05/08 update"

--- covid_19_africa.R
+++ covid_19_africa.R
@@ -49,8 +49,8 @@ covid_africa
 # at the time of uploading this script only 52 of the 54 African countries had confirmed cases
 length(unique(covid_africa$Country))
 
-# adding the daily update from John Hopkins University. adjust the name of the csv "05-06-2020.csv" accordingly
-daily_update <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-06-2020.csv")
+# adding the daily update from John Hopkins University. adjust the name of the csv "05-07-2020.csv" accordingly
+daily_update <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-07-2020.csv")
 
 # modifications where made to some names to match those in the daily updates from JHU
 africa_1 <- c("Algeria", "Angola", "Benin", "Botswana", "Burkina Faso",
@@ -89,7 +89,7 @@ daily_update <- daily_update %>%
  
 # correction on date
 daily_update <- daily_update %>%
-        mutate(ObservationDate = "2020-05-06") %>%
+        mutate(ObservationDate = "2020-05-07") %>%
         mutate(ObservationDate = as_date(ObservationDate))
 
 # adding african regions
