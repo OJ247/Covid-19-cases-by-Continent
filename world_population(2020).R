5a99218ee5b97b0f00da14705e51d9b542e2dc83
julokr <amjuliusokwir@gmail.com>
2020-10-18 22:23:37 +0300

    "update"

--- world_population(2020).R
+++ world_population(2020).R
@@ -1,19 +1,35 @@
-# loading the required packages
-suppressMessages(library(dplyr))
+# Description -------------------------------------------------------------
+
+# This R script scrapes world population data from 
+# https://www.worldometers.info/world-population/population-by-country/
+# and stores it as a data frame
+
+# Load packages -----------------------------------------------------------
+
+suppressMessages(library(tidyverse))
 suppressMessages(library(rvest))
 
-# getting the 2020 population data
+# Scrape -------------------------------------------------------------
+
 world_population <- read_html("https://www.worldometers.info/world-population/population-by-country/") %>%
         html_nodes(xpath = '//*[@id="example2"]') %>%
         html_table()
 
-# converting from list to dataframe
+# Wrangle ------------------------------------------------------------
+
+# convert list to data frame
 world_population <- world_population[[1]]
 
-# converting to a tibble and removing undesired column
-world_population <- tbl_df(world_population) %>%
+# convert to a tibble and remove undesired columns
+world_population <- as_tibble(world_population) %>%
         select(-`#`) %>%
         arrange(`Country (or dependency)`)
 
-# store file as a csv
+# remove commas from cols
+world_population %>% 
+        mutate(across(`Population (2020)`:`World Share`, ~ str_replace_all(.x, ",", "")))
+
+
+# Save --------------------------------------------------------------------
+
 write_csv(world_population, "World_population(2020).csv")
