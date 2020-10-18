4fa3d974a0eb5e37cbe5acb5676eb429b638cd04
julokr <amjuliusokwir@gmail.com>
2020-10-18 22:03:02 +0300

    "update"

--- covid19_tests.R
+++ covid19_tests.R
@@ -1,61 +1,68 @@
-# loading the required packages
-library(dplyr)
+# Description -------------------------------------------------------------
+
+# This function scrapes Covid-19 test data from https://www.worldometers.info/coronavirus/
+#
+# use: scrape_tests(test_date) 
+# where; test_date = today() - 2 to scrape test data from the "2 Days Ago" tab 
+#        OR
+#        test_date = today() - 1 to scrape test data from the "Yesterday" tab
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
 library(lubridate)
 library(rvest)
 
+# Function ----------------------------------------------------------------
 
-# getting the data from "2 days ago" tab
-covid19_data <- read_html("https://www.worldometers.info/coronavirus/") %>%
-        html_nodes(xpath = '//*[@id="main_table_countries_yesterday2"]') %>%
-        html_table()
+scrape_tests <- function(test_date) {
+        
+        # path to test_data
+        if (test_date == today() - 2) {
+                test_data <- read_html("https://www.worldometers.info/coronavirus/") %>%
+                        html_nodes(xpath = '//*[@id="main_table_countries_yesterday2"]') %>%
+                        html_table()
+        } else if (test_date == today() - 1) {
+                test_data <- read_html("https://www.worldometers.info/coronavirus/") %>%
+                        html_nodes(xpath = '//*[@id="main_table_countries_yesterday"]') %>%
+                        html_table()
+        } else {
+                stop("Incorrect date or date format. Enter correct date in the format 'YY-MM-DD'")
+        }
+        
+        test_data <- test_data[[1]]
+        
+        # select ONLY Covid19 test information
+        test_data <- test_data %>%
+                as_tibble() %>%
+                filter(!is.na(`#`)) %>%
+                select(`Country,Other`, `TotalTests`, `Population`, `Tests/1M pop`, `1 Testevery X ppl`) %>%
+                arrange(`Country,Other`)
+        
+        # convert unavailable observations into NA
+        test_data <- na_if(test_data, "")
+        
+        # add the date
+        test_data <- test_data %>%
+                mutate(Date = {{ test_date }}) %>%
+                select(Date, everything())
+        
+        # remove commas from cols and change cols to desired classes
+        test_data <- test_data %>%
+                mutate(
+                        across(TotalTests:`1 Testevery X ppl`, ~str_replace_all(.x, ",", "")),
+                        across(TotalTests:`1 Testevery X ppl`, as.integer)
+                )
+        
+        # save
+        test_data <- write_csv(test_data, "test_data.csv")
+        
+        # assign to global env and return test_data
+        assign("test_data", test_data, envir = globalenv())
+        test_data
+}
 
-# converting from list to data frame
-covid19_data <- covid19_data[[1]]
 
-# selecting ONLY covid19 test information
-covid19_tests <- covid19_data %>%
-        tbl_df() %>%
-        filter(!is.na(`#`)) %>%
-        select(`Country,Other`, `TotalTests`, `Population`, `Tests/1M pop`, `1 Testevery X ppl`) %>%
-        arrange(`Country,Other`)
 
-# minimal data wrangling
-# turn all unaviable observations into NA
-covid19_tests <- na_if(covid19_tests,"")
-
-# adding the date
-covid19_tests <- covid19_tests %>%
-        mutate(Date = today() - 2) %>%
-        select(Date, everything())
-
-# gettig the data from the "yesterday" tab
-yesterday <-  read_html("https://www.worldometers.info/coronavirus/") %>%
-        html_nodes(xpath = '//*[@id="main_table_countries_yesterday"]') %>%
-        html_table()
-
-# converting from list to data frame
-yesterday <- yesterday[[1]]
-
-# selecting ONLY covid19 test information
-yesterday <- yesterday %>%
-        tbl_df() %>%
-        filter(!is.na(`#`)) %>%
-        select(`Country,Other`, `TotalTests`, `Population`, `Tests/1M pop`, `1 Testevery X ppl`) %>%
-        arrange(`Country,Other`)
-
-# minimal data wrangling
-# turn all unaviable observations into NA
-yesterday <- na_if(yesterday, "")
-
-# adding the date
-yesterday <- yesterday %>%
-        mutate(Date = today() - 1) %>%
-        select(Date, everything())
-
-# append to covid19_tests data
-covid19_tests <- rbind(covid19_tests, yesterday)
-
-# save file
-write_csv(covid19_tests, "covid19_tests.csv")
 
 
